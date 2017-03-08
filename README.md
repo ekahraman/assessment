999e52488337924031a7ff177679488735c71288
Emre <ekahraman89@gmail.com>
2017-03-08 13:45:23 +0300

    Basic Caching and Some Styles added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# assessment
+# MobilDev small Assessment
+@author Emre KAHRAMAN
+
+----------------------------- DB -------------------------------
+
+CREATE TABLE  `books` (
+`id` INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`book_name` VARCHAR( 100 ) NOT NULL ,
+`author_name` VARCHAR( 50 ) NOT NULL ,
+`isbn` VARCHAR( 50 ) NOT NULL
+) ENGINE = MYISAM ;
+-----------------------------END DB------------------------------
+Note: charsets are all utf8_turkish_ci
--- ajax/read.php
+++ ajax/read.php
@@ -7,12 +7,10 @@ $object = new DB_OPS();
 // Design initial table header
 $data   = '<table class="table table-hover table-responsive">
                         <tr>
-                            <th>Record No</th>
                             <th>Book Name</th>
                             <th>Author Name</th>
                             <th>ISBN No</th>
-                            <th>Update</th>
-                            <th>Delete</th>
+                            <th>Update / Delete</th>
                         </tr>';
 
 
@@ -21,25 +19,20 @@ $books = $object->Read();
 
 // Append all users to the data table
 if (count($books) > 0) {
-    $i = 1;
     foreach ($books as $book) {
         $data .= '<tr>
-                    <td>' . $i .                    '</td>
                     <td>' . $book['book_name'] .    '</td>
                     <td>' . $book['author_name'] .  '</td>
                     <td>' . $book['isbn'] .         '</td>
                     <td>
-                        <button onclick="bookDetails(' . $book['id'] . ')" class="btn btn-warning">Update</button>
-                    </td>
-                    <td>
-                        <button onclick="deleteBook(' . $book['id'] . ')" class="btn btn-danger">Delete</button>
+                        <button onclick="bookDetails(' . $book['id'] . ')" class="btn btn-info"><i class="fa fa-refresh" aria-hidden="true"></i></button>
+                        <button onclick="deleteBook(' . $book['id'] . ')" class="btn btn-danger"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
                     </td>
                 </tr>';
-        $i++;
     }
 } else { // No books in database
     $data   .= '<tr>
-                    <td colspan="6">Not a single book in the database yet. Why don\'t you create one?</td>
+                    <td colspan="4"><p class="bg-danger">Not a single book in the database yet. Why don\'t you create one?</p></td>
                 </tr>';
 }
 
